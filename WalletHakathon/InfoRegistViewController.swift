b27147d853e9a30e3785a17895d9b84e9ffe4d15
Flatout73 <fantom2259@gmail.com>
2017-06-27 01:02:18 +0300

    Add registration

---
+++ WalletHakathon/InfoRegistViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InfoRegistViewController.swift
+//  WalletHakathon
+//
+//  Created by Леонид Лядвейкин on 27.06.17.
+//  Copyright © 2017 HSE. All rights reserved.
+//
+
+import UIKit
+
+class InfoRegistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WalletHakathon/RegistrViewController.swift
@@ -0,0 +1,74 @@
+//
+//  RegistrViewController.swift
+//  WalletHakathon
+//
+//  Created by Леонид Лядвейкин on 26.06.17.
+//  Copyright © 2017 HSE. All rights reserved.
+//
+
+import UIKit
+
+class RegistrViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var numberTF: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        numberTF.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func sendSMSForRegistration(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Смс отправлено!", message: "Введите код из СМС", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.text = "1234"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, alert] (_) in
+            let textField = alert.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(textField.text)")
+            
+            if(textField.text != "1234") {
+                self?.errorLabel.text = "Неверный код"
+                self?.errorLabel.isHidden = false
+            }
+            else {
+                self?.errorLabel.text = "Код подтвержден"
+                self?.performSegue(withIdentifier: "addInfo", sender: self)
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
